8b4b2cab3b47aa078faabe8a4a9ace5f97eae46f
samyhanyoon8-boop <samyhanyoon8@gmail.com>
2025-11-15 13:31:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 function buy(product, price, data) {
-  let message = `🚀 طلب شراء\n\nالمنتج: ${product}\nالسعر: ${price}`;
+    let message = `🔵 طلب شراء\n------------------\nالمنتج: ${product}\nالسعر: ${price}`;
 
-  if (data && data.trim() !== "") {
-    message += `\nالبيانات: ${data}`;
-  }
+    if (data && data.trim() !== "") {
+        message += `\nالبيانات: ${data}`;
+    }
 
-  let encoded = encodeURIComponent(message);
+    let encoded = encodeURIComponent(message);
 
-  // رقم الواتساب
-  let phone = "201284070117";
+    // رقم الواتساب
+    let phone = "201284070117";
 
-  // لينك التيلجرام
-  let telegramUser = "K_A_P_OO7";
+    // يوزر التليجرام
+    let telegramUser = "K_A_P_OO7";
 
-  // يسأل الزبون يفتح إيه
-  let choose = confirm("اضغط OK للواتساب\nواضغط Cancel للتليجرام");
+    // اختيار واتساب أو تليجرام
+    let choose = confirm("اضغط OK للشراء عبر واتساب\nاضغط Cancel للشراء عبر تليجرام");
 
-  if (choose) {
-    // واتساب
-    window.open(`https://wa.me/${phone}?text=${encoded}`);
-  } else {
-    // تيلجرام
-    window.open(`https://t.me/${telegramUser}?text=${encoded}`);
-  }
+    if (choose) {
+        // واتساب
+        window.open(`https://wa.me/${phone}?text=${encoded}`);
+    } else {
+        // تليجرام
+        window.open(`https://t.me/${telegramUser}?text=${encoded}`);
+    }
 }
